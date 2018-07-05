8ffe66300ce89bf5bce769ace1a7608f2b6e2185
Robin Thrift <services@robinthrift.com>
2018-07-05 15:43:27 +0200

    feat(GUI): autoresizing message input

--- Sources/Controllers/ChatViewController.swift
+++ Sources/Controllers/ChatViewController.swift
@@ -3,7 +3,14 @@ import EntropyKit
 
 class ChatViewController: NSViewController {
     @IBOutlet private var tableView: NSTableView!
-    @IBOutlet private var messageView: NSTextView!
+    @IBOutlet private var messageInput: NSTextView!
+    @IBOutlet private var messageInputHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private var messageInputMinHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private var messageInputMaxHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private var messageInputBackgroundView: NSVisualEffectView!
+
+    private var newLineModifier = false
+    private var lastTextHeight: CGFloat = 0
 
     private var messages: ObservablePersistedList<Message>?
     private var observerToken: ObservablePersistedList<Message>.ObserverToken?
@@ -31,7 +38,11 @@ class ChatViewController: NSViewController {
         tableView.dataSource = self
         tableView.delegate = self
 
-        messageView.delegate = self
+        messageInput.delegate = self
+        messageInput.enclosingScrollView?.wantsLayer = true
+        messageInput.enclosingScrollView?.layer?.cornerRadius = 5.0
+        messageInput.textContainerInset = NSSize(width: 4, height: 4)
+        messageInput.isVerticallyResizable = true
 
         let nib = NSNib(nibNamed: "TextMessageView", bundle: Bundle.main)
         tableView.register(nib, forIdentifier: NSUserInterfaceItemIdentifier(rawValue: "TextMessageView"))
@@ -54,9 +65,11 @@ extension ChatViewController {
     private func send() {
         guard let room = room else { return }
 
-        let body = messageView.string.trimmingCharacters(in: .whitespacesAndNewlines)
+        let body = messageInput.string.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard !body.isEmpty else { return }
+
         Entropy.default.sendMessage(room: room, body: body)
-        messageView.string = ""
+        messageInput.string = ""
     }
 }
 
@@ -93,9 +106,44 @@ extension ChatViewController: NSTableViewDelegate {
 // MARK: - NSTextViewDelegate
 
 extension ChatViewController: NSTextViewDelegate {
+    private func updateMessageInputLayout() {
+        // These could maybe be cached, but they are unowned bindings and it's unclear, when they are set and if they might change, so this is probably safer
+        guard let textContainer = messageInput.textContainer, let layoutManager = messageInput.layoutManager else { return }
+
+        // This could be optimized. But we don't have a lot of control over it, so I'm very cautions.
+        layoutManager.ensureLayout(for: textContainer)
+        let height = layoutManager.usedRect(for: textContainer).height
+
+        // We could add another check here to avoid unnecessary updates to auto layout.
+        guard height != lastTextHeight else { return }
+
+        lastTextHeight = height
+
+        if height < messageInputMinHeightConstraint.constant {
+            messageInputHeightConstraint.constant = messageInputMinHeightConstraint.constant
+        } else if height > messageInputMaxHeightConstraint.constant {
+            messageInputHeightConstraint.constant = messageInputMaxHeightConstraint.constant
+        } else {
+            messageInputHeightConstraint.constant = height + messageInput.textContainerInset.height * 2 // * 2 because top and bottom, I think. Anyway, this works... for now.
+        }
+
+        messageInputBackgroundView.layoutSubtreeIfNeeded()
+        tableView.enclosingScrollView?.contentInsets.bottom = messageInputBackgroundView.frame.height
+    }
+
+    func textDidChange(_: Notification) {
+        updateMessageInputLayout()
+    }
+
+    override func flagsChanged(with event: NSEvent) {
+        super.flagsChanged(with: event)
+        newLineModifier = event.modifierFlags.contains(.shift) || event.modifierFlags.contains(.control) || event.modifierFlags.contains(.option)
+    }
+
     func textView(_: NSTextView, doCommandBy commandSelector: Selector) -> Bool {
-        if commandSelector == #selector(insertNewline) {
+        if commandSelector == #selector(insertNewline) && !newLineModifier {
             send()
+            updateMessageInputLayout()
             return true
         } else {
             return false
